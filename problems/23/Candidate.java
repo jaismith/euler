c76f2fa3b20730e819d1c294b36992a76573b647
jaismith <jksmithnyc@gmail.com>
2018-09-19 14:04:43 -0400

    working on 23

---
+++ problems/23/Candidate.java
@@ -0,0 +1,67 @@
+//euler 23
+//abundant number candidates
+
+//imports
+import java.util.ArrayList;
+
+public class Candidate {
+     //variables
+     int number;
+     ArrayList<Integer> divisorList = new ArrayList<Integer>();
+     int sum = 0;
+
+     //construct
+     public Candidate(int number) {
+          this.number = number;
+
+          //find divisors
+          for(int i = 1; i < (number / 2) + 1; i++) {
+               if(!divisorList.contains(i)) {
+                    if(number % i == 0) {
+                         divisorList.add(i);
+                    }
+               }
+          }
+
+          //add each divisor to sum
+          for(int i : divisors()) {
+               sum += i;
+          }
+     }
+
+     public ArrayList<Integer> divisors() {
+          //return output
+          return divisorList;
+     }
+
+     public int divisorSum() {
+          //return output
+          return sum;
+     }
+
+     //provide integer equivalent of candidate
+     public int toInteger() {
+          return number;
+     }
+
+     //make candidates comparable
+     public int compareTo(Candidate n) {
+          if(number < n.toInteger()) {
+               return -1;
+          } else if (number == n.toInteger()) {
+               return 0;
+          } else {
+               return 1;
+          }
+     }
+
+     public String isType() {
+          if(number < sum) {
+               return "deficient";
+          } else if (number == sum) {
+               return "perfect";
+          } else {
+               return "abundant";
+          }
+     }
+}
---
+++ problems/23/Main.java
@@ -0,0 +1,14 @@
+//main class for question 23
+
+class Main {
+     public static void main(String[] args) {
+          //testing Candidate class
+          for(int i = 1; i <= 28123; i++) {
+               Candidate test = new Candidate(i);
+
+               //System.out.println(i + ": " + test.isType());
+          }
+
+          System.out.println("done");
+     }
+}
